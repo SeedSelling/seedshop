c0321682d7c830649a85ea30a87cdc4c3b2f6c5b
SeedSelling <aartijune2001@gmail.com>
2026-02-27 14:57:21 +0530

    Update wsgi.py

--- seedshop/seedshop/wsgi.py
+++ seedshop/seedshop/wsgi.py
@@ -1,16 +1,16 @@
 """
 WSGI config for seedshop project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
-
 import os
+import sys
+
+# Render deployment के लिए path fix
+sys.path.append('/opt/render/project/src')
+
+# Settings module set करो
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seedshop.settings')
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seedshop.settings')
-
 application = get_wsgi_application()
+
